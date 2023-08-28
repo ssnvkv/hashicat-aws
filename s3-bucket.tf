26daed3d5ff17276e9cd58786ed4ea3b4db7c234
ssnvkv <ssnvkv@gmail.com>
2023-08-28 22:26:14 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,8 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "ssnvkv-test-s3-bucket"
   acl    = "private"
-  bucket_prefix = "ssnvkv-test"
 
   versioning = {
     enabled = true
